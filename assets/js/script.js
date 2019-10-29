738fd3cbf229fb8a27b1f94018b9db1e6f76de2a
Kevin Rouwkema <kevinrouwkema@gmail.com>
2019-10-29 22:53:18 +0100

    Created extra rows, added styling and first steps of the js part.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 let arr = [];
 
-let items = document.getElementsByClassName("grid-item");
+let container = document.getElementsByClassName("grid");
 
-for (var i = 0; i < items.length; i++) {
-  let value = items[i].dataset.value;
 
-  if(value == '') {
-    let newValue = i + 1;
-    items[i].innerHTML = newValue;
-    arr.push(parseFloat(newValue));
-  } else {
-      items[i].innerHTML = value;
-      arr.push(parseFloat(value));
+for (var c = 1; c <= container.length; c++) {
+
+  let items = $(`.sudoku-${c} .grid-item`);
+  for (var i = 0; i < items.length; i++) {
+    let value = items[i].dataset.value;
+
+    if(value == '') {
+      let newValue = i + 1;
+      items[i].innerHTML = newValue;
+      arr.push(parseFloat(newValue));
+    } else {
+        items[i].innerHTML = value;
+        arr.push(parseFloat(value));
+    }
   }
 }
